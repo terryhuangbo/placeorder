c32b2117fe67da3186e6e664c1db3a22cdd3add8
terry <terry1987101@163.com>
2016-10-12 23:37:56 +0800

    【开发】修改数据库配置

--- common/config/db.php
+++ common/config/db.php
@@ -8,11 +8,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=139.196.230.61;dbname=orders',
-//    'dsn' => 'mysql:host=127.0.0.1;dbname=acc',
     'username' => 'orders',
-//    'username' => 'root',
     'password' => 'orders626@',
-//    'password' => '',
     'tablePrefix' => 'tb_',
     'charset' => 'utf8'
 ];
