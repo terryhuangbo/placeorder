c68f67615193c7132dffbfb1f3b994690ad5bb08
bhuang <3121045133@qq.com>
2016-11-22 08:39:04 +0800

    【优化】修改默认缓存

--- common/config/main.php
+++ common/config/main.php
@@ -9,19 +9,7 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\MemCache',
-            'servers' => [
-                [
-                    'host' => '127.0.0.1',
-                    'port' => 11211,
-                    'weight' => 60,
-                ],
-                [
-                    'host' => '127.0.0.1',
-                    'port' => 11211,
-                    'weight' => 40,
-                ],
-            ],
+            'class' => 'yii\caching\FileCache',
         ],
         'urlManager' => [
             'enablePrettyUrl' => true,
