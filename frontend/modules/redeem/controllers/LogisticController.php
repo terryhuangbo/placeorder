51240a0ec4d66af18ff82e2657c2481f688116b7
Huangbo <huangbo@yunshan.net.cn>
2017-03-31 15:27:07 +0800

    hb6

--- frontend/modules/redeem/controllers/LogisticController.php
+++ frontend/modules/redeem/controllers/LogisticController.php
@@ -8,7 +8,7 @@ use common\lib\Http;
 class LogisticController extends BaseController
 {
     public $layout = 'layout';
-    public $enableCsrfValidation = false;;
+    public $enableCsrfValidation = false;
     private $_apikey = 'apikey:aa189f2a5edc7813767ca14ca206640b'; //物流接口
 
     /**
@@ -16,9 +16,9 @@ class LogisticController extends BaseController
      *
      * @return type
      */
-    public function actionDetail()  
+    public function actionDetail()
     {
-        $this->render('detail');;
+        $this->render('detail');
         exit;
     }
 
