4ac914de2ba752bf3348b66e7cfb860b81b53a60
Huangbo <huangbo@yunshan.net.cn>
2017-04-06 12:46:06 +0800

    up1

--- frontend/modules/redeem/controllers/MyController.php
+++ frontend/modules/redeem/controllers/MyController.php
@@ -2,8 +2,6 @@
 
 namespace frontend\modules\redeem\controllers;
 
-use Yii;
-use yii\helpers\ArrayHelper;
 use app\base\BaseController;
 use common\models\User;
 use common\models\Order;
@@ -14,24 +12,30 @@ class MyController extends BaseController
 {
     public $layout = 'layout';
     public $enableCsrfValidation = false;
+
     public function init()
     {
         $this->_uncheck = [];
     }
+
     /**
-     * 个人中心
+     * 个人中心.
+     *
      * @return type
      */
-    public function actionIndex() {
+    public function actionIndex()
+    {
         $_data = [
-            'user' => $this->user
+            'user' => $this->user,
         ];
+
         return $this->render('index', $_data);
     }
 
-
     /**
      * 我的地址
+     * up1.
+     *
      * @return type
      */
     public function actionAddress()
@@ -47,11 +51,13 @@ class MyController extends BaseController
             'list' => $list,
             'uid' => $this->uid,
         ];
+
         return $this->render('address', $_data);
     }
 
     /**
-     * 我的积分
+     * 我的积分.
+     *
      * @return type
      */
     public function actionPoints()
@@ -60,29 +66,33 @@ class MyController extends BaseController
         $record_list = $pr_mdl->_get_list(['uid' => $this->uid], 'id DESC');
         $_data = [
             'user' => $this->user,
-            'record_list' => $record_list
+            'record_list' => $record_list,
         ];
+
         return $this->render('points', $_data);
     }
 
     /**
-     * 我的订单
+     * 我的订单.
+     *
      * @return type
      */
     public function actionOrder()
     {
         $r_mdl = new Order();
-        $_order_list = $r_mdl->_get_list([Order::tableName() . '.uid' => $this->uid], 'oid DESC');
+        $_order_list = $r_mdl->_get_list([Order::tableName().'.uid' => $this->uid], 'oid DESC');
         $_status_list = $r_mdl::_get_status_list();
         $_data = [
             'order_list' => $_order_list,
             'status_list' => $_status_list,
         ];
+
         return $this->render('order', $_data);
     }
 
     /**
-     * 异步加载订单
+     * 异步加载订单.
+     *
      * @return type
      */
     public function actionAjaxLoadOrder()
@@ -107,7 +117,7 @@ class MyController extends BaseController
             $_order_list[$key]['time'] = date('Y/m/d', $val['create_at']);
             $_order_list[$key]['status_name'] = getValue($_status_list, [$val['order_status']], '');
             $_order_list[$key]['link'] = in_array($val['order_status'], [Order::STATUS_PAY, Order::STATUS_SEND])
-                ? 'javaScript:void(0)' : '/redeem/logestic/detail?oid=' . $val['oid'];
+                ? 'javaScript:void(0)' : '/redeem/logestic/detail?oid='.$val['oid'];
         }
         $_data = [
             'order_list' => $_order_list,
